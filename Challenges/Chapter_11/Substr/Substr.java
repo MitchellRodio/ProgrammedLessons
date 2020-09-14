b3118ac0b5bb7a88ae7ba22c2da52e89612bc98f
Mitchell Rodio <mitchellrodio@gmail.com>
2020-09-13 20:31:25 -0600

    Add substr

---
+++ Challenges/Chapter_11/Substr/Substr.java
@@ -0,0 +1,12 @@
+public class Substring {
+
+    public static void main ( String[] args ) {
+    String str = new String( "Lol" ); 
+
+    String sub = str.substring(7); 
+
+    System.out.println( sub );
+
+     }
+
+}
