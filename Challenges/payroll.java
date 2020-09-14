541cd94e3b06d5cbd2caae524d3eae63bf7ec3d5
Mitchell Rodio <mitchellrodio@gmail.com>
2020-09-13 20:04:06 -0600

    add PayRoll

---
+++ Challenges/payroll.java
@@ -0,0 +1,11 @@
+public class Payroll {
+    public static void main ( String[] args )
+    {
+        long hoursWorked = ;
+
+        double payRate      = 10.0, taxRate = 0.10;
+        System.outprintln("Hours Worked: " + hoursWorked);
+        System.outprintln("pay Amount : " + (hoursWorked * payRate) );
+        System.outprintln("tax Amount : " + (hoursWorked * payRate * taxRate) );
+    }
+}
