fc6ddd2bccd8e7542b134f5bbd51850c10ec9e7e
Mitchell Rodio <56127734+MitchellRodio@users.noreply.github.com>
2020-11-03 12:27:30 -0700

    Edit Wedge Of Stars

--- Challenges/Chapter_20/WedgeOfStars.java
+++ Challenges/Chapter_20/WedgeOfStars.java
@@ -9,7 +9,7 @@ public static void main (String[] args )
   System.out.print( "How many Stars do you want? " );
   int starCount = sc.nextInt() ;
   while(starCount>0) {
-     //This bit below is what prints a line of stars.
+    
      for(int i = 1; i <= starCount; i++) {
          System.out.print("*");
      }
